6684fc3935d876675796917ac50e50d165d4edb9
Prof Pensamento <129297547+ProfTiagoCaser@users.noreply.github.com>
2026-06-12 08:48:52 -0300

    Add files via upload

---
+++ script - Copia.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+    mensagem.textContent = "A agrofloresta ajuda a produzir alimentos preservando o solo, a água e a biodiversidade.";
+});
